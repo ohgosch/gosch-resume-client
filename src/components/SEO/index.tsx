56b514f4590c955c2f10bf18f52eace7c0cef0a1
Gosch <gosch@gosch.io>
2022-05-12 14:51:06 -0300

    Update SEO rules and image preview

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,4 +1,6 @@
+import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
+
 import config from 'config';
 
 type SEOProps = {
@@ -8,19 +10,36 @@ type SEOProps = {
 
 const SEO = ({ description, title }: SEOProps) => {
   const siteTitle = config.title;
+  const { t } = useTranslation();
 
   return (
     <Head>
       <title>{`${title ? `${title} - ` : ''}${siteTitle}`}</title>
-      <meta name="description" content={description} />
+      <meta
+        name="description"
+        content={description ? description : t('description')}
+      />
+      <meta name="author" content={config.author.name} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="/assets/img/seo/linkedin-preview.png"
+      />
+      <meta
+        property="og:description"
+        content={description ? description : t('description')}
+      />
       <meta property="og:site_name" content={siteTitle} />
+
+      {/* Twitter */}
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:creator" content={config.social.twitter} />
       <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
+      <meta
+        property="twitter:description"
+        content={description ? description : t('description')}
+      />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
