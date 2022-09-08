0755d332eb5ae23018d1bca2aabfc89388db500a
Gosch <gosch@gosch.io>
2022-09-08 20:08:44 -0300

    Fix locale of ptBR in dates

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,9 @@
 import { format as formatLib, parseISO } from 'date-fns';
+import { enUS, ptBR } from 'date-fns/locale';
 
-export const formatDate = (date = '', format = 'yyyy-MM-dd') => {
+export const formatDate = (date = '', format = 'yyyy-MM-dd', locale = 'en') => {
   if (!date) return date;
-  return formatLib(parseISO(date), format);
+  return formatLib(parseISO(date), format, {
+    locale: locale === 'pt' ? ptBR : enUS,
+  });
 };
